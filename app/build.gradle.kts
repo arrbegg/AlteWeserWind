9268016f3a5298e9b4988f6a6f5d6b955ca87df4
arrbegg <10772700+arrbegg@users.noreply.github.com>
2025-08-19 13:41:39 +0200

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+}
+
+android {
+  namespace = "dev.max.alteweserwind"
+  compileSdk = 34
+
+  defaultConfig {
+    applicationId = "dev.max.alteweserwind"
+    minSdk = 24
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
+    }
+  }
+}
+
+dependencies {
+  implementation("androidx.core:core-ktx:1.13.1")
+  implementation("androidx.appcompat:appcompat:1.7.0")
+  implementation("com.google.android.material:material:1.12.0")
+}
